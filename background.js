d98dd1ae6ff884ad7a68a515a3d769b1de2be7fd
gtkesh <gtkesh@gmail.com>
2013-10-05 14:38:31 -0400

    translate method added

--- background.js
+++ background.js
@@ -1,10 +1,12 @@
-function InstantTranslate() {
+function InstantTranslate(){
+
 }
 
 InstantTranslate.prototype = {
 
   onInputEntered: function(text) {
     console.log('inputEntered: ' + text);
+
   },
 
   onInputChanged: function(text, suggest) {
@@ -17,6 +19,19 @@ InstantTranslate.prototype = {
       event.preventDefault();
     };
     document.execCommand("Copy", false, null);
+  },
+
+  translate: function(from, to, phrase){
+  	var key = ' ';
+  	var query = 'https://www.googleapis.com/language/translate/v2?key=' + key +
+  				'&source=' + from + 
+  				'&target=' + to + 
+  				'callback=translateText&q=' + phrase;
+
+  },
+  translateText: function(response){
+  	var translated = response.data.translations[0].translatedText;
+  	console.log(translated);
   }
 
 }
